81fb9346a471d6435c284d8d30d64020ecc77a0c
delano <delanoyusufhabibie47@gmail.com>
2024-07-24 15:49:00 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:final_project/screens/home_screen.dart';
+import 'package:final_project/widgets/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MinangXXI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colours.scaffoldBgColor,
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:final_project/widgets/movies_slider.dart';
+import 'package:final_project/widgets/trending_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Image.asset(
+          'assets/minangxxi.png',
+          fit: BoxFit.cover,
+          height: 55,
+          filterQuality: FilterQuality.high,
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Trending Movies',
+                style: GoogleFonts.aBeeZee(fontSize: 25),
+              ),
+              const SizedBox(height: 32),
+              const TrendingSlider(),
+              const SizedBox(height: 32),
+              Text(
+                'Top Rated Movies',
+                style: GoogleFonts.aBeeZee(
+                  fontSize: 25,
+                ),
+              ),
+              const SizedBox(height: 32),
+              const MoviesSlider(),
+              const SizedBox(height: 32),
+              Text(
+                'Upcoming Movies',
+                style: GoogleFonts.aBeeZee(
+                  fontSize: 25,
+                ),
+              ),
+              const SizedBox(height: 32),
+              const MoviesSlider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
